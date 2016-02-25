38014c9200d37674869901d96c9f990870beda6d
Jiayao Wu <motevans412@gmail.com>
2016-02-25 11:37:58 +0800

    Update SoftInput.js

--- SoftInput.js
+++ SoftInput.js
@@ -11,68 +11,58 @@ import React, {
 } from 'react-native';
 
 export default class SoftInput extends Component {
-
 	constructor(props) {
 		super(props)
-
-    this.state = {
-        keyboardSpace: 0,
-    };
-
+		this.state = {
+			keyboardSpace: 0,
+		};
 		this.updateKeyboardSpace = this.updateKeyboardSpace.bind(this);
-   	this.resetKeyboardSpace = this.resetKeyboardSpace.bind(this);
+		this.resetKeyboardSpace = this.resetKeyboardSpace.bind(this);
 	}
-
- updateKeyboardSpace(frames) {
-    if (!frames.endCoordinates)
-        return;
-    this.setState({
-        keyboardSpace: frames.endCoordinates.height
-    });
-  }
-
-  resetKeyboardSpace() {
-    this.setState({
-        keyboardSpace: 0
-    });
-  }
-
-  componentDidMount() {
-    this._listeners = [
-        DeviceEventEmitter.addListener('keyboardWillShow', this.updateKeyboardSpace),
-        DeviceEventEmitter.addListener('keyboardWillHide', this.resetKeyboardSpace)
-    ];
-  }
-
-  componentWillUnmount() {
-    this._listeners.forEach(function(listener) {
-        listener.remove();
-    });
-  }
-  
+	updateKeyboardSpace(frames) {
+		if (!frames.endCoordinates)
+		    	return ;
+		this.setState({
+		    	keyboardSpace: frames.endCoordinates.height
+		});
+	}
+	resetKeyboardSpace() {
+		this.setState({
+			keyboardSpace: 0
+		});
+	}
+	componentDidMount() {
+		this._listeners = [
+			DeviceEventEmitter.addListener('keyboardWillShow', this.updateKeyboardSpace),
+			DeviceEventEmitter.addListener('keyboardWillHide', this.resetKeyboardSpace),
+		];
+	}
+	componentWillUnmount() {
+		this._listeners.forEach(function(listener) {
+		    	listener.remove();
+		});
+	}
   /*
     this.props.scroll: Scroll method with scrollview : (offset) => { this.refs.scrollView.scrollTo(offset) }
   */
 	onFocus() {
 		const { scroll } = this.props;
 		this.textInput.measure((ox, oy, width, height, px, py) => {
-			console.log(ox, oy, width, height, px, py)
 			scroll(py - this.state.keyboardSpace + 180)
-		})
+		});
 	}
-
 	onBlur() {
 		const { scroll } = this.props;
-		scroll(0)
+		scroll(0);
 	}
 
 	render() {
 		const { 
-			keyboardType, 
-			onChangeText, 
-			placeholder, 
+			keyboardType,
+			onChangeText,
+			placeholder,
 			secureTextEntry,
-			multiline, 
+			multiline,
 			value,
 			style,
 		} = this.props;
@@ -108,9 +98,7 @@ export default class SoftInput extends Component {
 					placeholder={placeholder}/>
 			)
 		}
-
 	}
-
 }
 
 const styles = StyleSheet.create({
@@ -118,12 +106,12 @@ const styles = StyleSheet.create({
 		flex: 1,
 		height: 40,
 		fontSize: 16,
-		backgroundColor: '#FFFFFF'
+		backgroundColor: '#FFFFFF',
 	},
 	inputIOS: {
 		flex: 1,
 		height: 40,
 		fontSize: 14,
-		backgroundColor: '#FFFFFF'
+		backgroundColor: '#FFFFFF',
 	}
 })
